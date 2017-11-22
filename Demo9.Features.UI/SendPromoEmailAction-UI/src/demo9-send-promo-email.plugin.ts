c4ff115021f07c95f53c1ed36b02c1aacb50bcb9
brimit-account <a.vershalovich@brimit.com>
2017-11-22 16:52:28 +0300

    UI files

---
+++ Demo9.Features.UI/SendPromoEmailAction-UI/src/demo9-send-promo-email.plugin.ts
@@ -0,0 +1,18 @@
+﻿import { Plugin } from '@sitecore/ma-core';
+import { SendPromoEmailActivity } from './demo9-send-promo-email/send-promo-email-activity';
+import { SendPromoEmailModuleNgFactory } from '../codegen/demo9-send-promo-email/send-promo-email-module.ngfactory';
+import { ReadonlyEditorComponent } from '../codegen/demo9-send-promo-email/editor/readonly-editor.component';
+
+// Use the @Plugin decorator to define all the activities the module contains.
+@Plugin({
+    activityDefinitions: [
+        {
+            // The ID must match the ID of the activity type description definition item in the CMS.
+            id: 'f8b0dffd-e3d3-4ea2-b3fa-8bfcaa4e41da', 
+            activity: SendPromoEmailActivity,
+            editorComponenet: ReadonlyEditorComponent,
+            editorModuleFactory: SendPromoEmailModuleNgFactory
+        }
+    ]
+})
+export default class SendPromoEmailPlugin {}
---
+++ Demo9.Features.UI/SendPromoEmailAction-UI/src/demo9-send-promo-email/send-promo-email-activity.ts
@@ -0,0 +1,25 @@
+﻿import { SingleItem} from '@sitecore/ma-core';
+
+export class SendPromoEmailActivity extends SingleItem {
+	
+    getVisual(): string {
+        const subTitle = 'Send promo email';
+        const cssClass = this.isDefined ? '' : 'undefined';
+        
+        return `
+            <div class="viewport-readonly-editor marketing-action ${cssClass}">
+                <span class="icon">
+                    <img src="/~/icon/OfficeWhite/32x32/mail_forward.png" />
+                </span>
+                <p class="text with-subtitle" title="Send Promo Email">
+                    Send Promo Email
+                    <small class="subtitle" title="${subTitle}">${subTitle}</small>
+                </p>
+            </div>
+        `;
+    }
+
+    get isDefined(): boolean {
+        return true;
+    }
+}
